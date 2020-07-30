7f0bbf11356ee77c900ea4f6f282ce2d5f5be3b0
Soohyun-CHOI <csh0sns@naver.com>
2020-07-31 01:38:56 +0900

    useRef

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 
 function InputSample() {
     const [inputs, setInputs] = useState({
@@ -6,6 +6,7 @@ function InputSample() {
         nickName: "",
     })
     const { name, nickName } = inputs;
+    const nameInput = useRef(); // DOM에 접근
 
     const onChange = (e) => {
         const { name, value } = e.target;
@@ -19,11 +20,12 @@ function InputSample() {
             name: "",
             nickName: "",
         })
+        nameInput.current.focus();
     }
 
     return (
         <div>
-            <input name="name" placeholder="이름" onChange={onChange} value={name} />
+            <input name="name" placeholder="이름" onChange={onChange} value={name} ref={nameInput} />
             <input name="nickName" placeholder="닉네임" onChange={onChange} value={nickName} />
             <button onClick={onReset}>초기화</button>
             <div>
