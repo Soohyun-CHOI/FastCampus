91953235d179325aef70f8f9c3319e1ac33087b4
Soohyun-CHOI <csh0sns@naver.com>
2020-08-01 11:35:46 +0900

    useRef

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react';
-import InputSample from "./InputSample";
+import React, {useRef} from 'react';
+import UserList from "./UserList";
 
 function App() {
+  const users = [
+        {
+            id: 1,
+            userName: "one",
+            email: "one@gmail.com",
+        },
+        {
+            id: 2,
+            userName: "two",
+            email: "two@gmail.com",
+        },
+        {
+            id: 3,
+            userName: "three",
+            email: "three@gmail.com",
+        }
+    ];
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+      console.log(nextId.current);
+      nextId.current += 1;
+  }
+
   return (
-      <InputSample />
+      <UserList users={users} />
   )
 }
 
---
+++ begin-react/src/UserList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function User({user}) {
+    return (
+        <div>
+            <b>{user.userName}</b> <span>({user.email})</span>
+        </div>
+    )
+}
+
+function UserList({ users }) {
+    return (
+        <div>
+            { users.map(user => <User user={user} key={user.id} />) }
+        </div>
+    )
+}
+
+export default UserList;
